48cf3c4f8a8ac93a855a495378baf5769927161c
Grant Jurgensen <gajurgensen@gmail.com>
2018-12-02 16:37:35 -0600

    Simpler and more efficient fold

--- 2018/p2/part2.rs
+++ 2018/p2/part2.rs
@@ -18,18 +18,24 @@ fn foo(s: &str) -> Option<String> {
     };
 
     for line in tail.lines() {
-        let zipped = head.chars().zip(line.chars()); // Fold is b -> a -> b rather than a -> b -> b :(
-        let (diffs, _, i) = zipped.fold((0, 0, 0), |(diffs, pos, i), (c1, c2)| {
+        let mut zipped = head.chars().zip(line.chars());
+        // Fold takes a function of type b -> a -> b rather than a -> b -> b  :(
+        // (or in the case of try_fold, b -> a -> Try<b>)
+        // try_fold stops at first error (None for Option)
+        let diffs = zipped.try_fold((0, 0), |(diffs, pos), (c1, c2)| {
             if c1 != c2 {
-                (diffs+1, pos+1, pos)
+                if diffs == 1 { None } else { Some((diffs+1, pos)) }
             } else {
-                (diffs, pos+1, i)
+                Some((diffs, pos+1))
             }
         });
-        if diffs == 1 {
+
+        if let Some((1, i)) = diffs {
             return Some(String::from(&head[0..i]) + &head[i+1..]);
         }
     }
 
+    // Apparently Rust doesn't do tail recursion optimization.
+    // This should be refactored into a loop
     foo(tail)
 }
