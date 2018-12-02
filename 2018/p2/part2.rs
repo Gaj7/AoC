f9e262c7b418112cca2fba850a723b8464223aaa
Grant Jurgensen <gajurgensen@gmail.com>
2018-12-02 16:47:39 -0600

    Change diff count to boolean

--- 2018/p2/part2.rs
+++ 2018/p2/part2.rs
@@ -23,15 +23,15 @@ fn foo(mut s: &str) -> Option<String> {
             // Fold takes a function of type b -> a -> b rather than a -> b -> b  :(
             // (or in the case of try_fold, b -> a -> Try<b>)
             // try_fold stops at first error (None for Option)
-            let diffs = zipped.try_fold((0, 0), |(diffs, pos), (c1, c2)| {
+            let diffs = zipped.try_fold((false, 0), |(found_diff, pos), (c1, c2)| {
                 if c1 != c2 {
-                    if diffs == 1 { None } else { Some((diffs+1, pos)) }
+                    if found_diff { None } else { Some((true, pos)) }
                 } else {
-                    Some((diffs, pos+1))
+                    Some((found_diff, pos+1))
                 }
             });
 
-            if let Some((1, i)) = diffs {
+            if let Some((true, i)) = diffs {
                 return Some(String::from(&head[0..i]) + &head[i+1..]);
             }
         }
